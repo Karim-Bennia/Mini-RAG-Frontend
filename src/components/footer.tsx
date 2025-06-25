0c6e4044db12d1ba3cd08409cb4f4def0a73f777
Karim Bennia <karim@swissdidata.com>
2025-06-25 09:58:11 +0100

    add footer for our app

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <footer className="border-t dark:border-gray-700">
+      <div className="container mx-auto px-4 py-4 text-center text-sm text-gray-600 dark:text-gray-400">
+      Chat with Your Files : © {new Date().getFullYear()} Turn every document into a living conversation. Upload, ask, and discover insights 
+          by the magic of RAG .
+      </div>
+    </footer>
+  );
+} 
